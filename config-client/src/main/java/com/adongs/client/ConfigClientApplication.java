f7d09db1b8619e4d2977eddca68fc41055aaa396
yudong <1286630069@qq.com>
2019-07-23 17:14:36 +0800

    init

--- config-client/src/main/java/com/adongs/client/ConfigClientApplication.java
+++ config-client/src/main/java/com/adongs/client/ConfigClientApplication.java
@@ -3,6 +3,8 @@ package com.adongs.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 public class ConfigClientApplication {
--- config-client/src/main/java/com/adongs/client/TestController.java
+++ config-client/src/main/java/com/adongs/client/TestController.java
@@ -1,6 +1,7 @@
 package com.adongs.client;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version 1.0
  */
 @RestController
+@RefreshScope
 public class TestController {
 
 
     @Value("${dome.name}")
     private String name;
 
+    @Value("${server.port}")
+    private String port;
+
+
     @GetMapping("test")
     public String test(){
-        return name;
+        return name+"   "+port;
     }
 
 }
